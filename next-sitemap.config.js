5c1656808705e25779e1301a6f1307b4100d2448
unknown <flahinounou@gmail.com>
2025-02-05 11:36:43 +0100

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,11 @@
-module.exports = {
-    siteUrl: 'https://la-verriere-menu.vercel.app/', // Replace with your website's URL
-    generateRobotsTxt: true, // Generate a robots.txt file
-    // Split large sitemaps into multiple files
-     // Exclude specific pages from the sitemap
-    changefreq: 'daily', // Set the default change frequency
-    priority: 0.7, // Set the default priority
-    transform: async (config, path) => {
-      return {
-        loc: path, // URL of the page
-        changefreq: config.changefreq,
-        priority: config.priority,
-        lastmod: new Date().toISOString(), // Add the last modified date
-      };
-    },
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://Laverieremenu.com', // Replace with your domain
+    generateRobotsTxt: true, // Generate robots.txt file
+    changefreq: 'daily', // Optional: Frequency of content updates
+    priority: 0.7, // Optional: Default priority
+    sitemapSize: 5000, // Optional: Max entries per sitemap file
   };
+  
+  module.exports = config;
+  
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withSitemap = require('next-sitemap');
 
-module.exports = withSitemap({
-  // Your existing Next.js configuration
-  reactStrictMode: true,
-  images: {
-    domains: ['Laverieremenu.com'], // Add your image domains here
-  },
-});
 const nextConfig = {};
 
 export default nextConfig;
