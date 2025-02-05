dc0845d617215bfb8b2103675fa0287715f4134c
unknown <flahinounou@gmail.com>
2025-02-05 12:05:04 +0100

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
     siteUrl: 'https://la-verriere-menu.vercel.app/', // Replace with your domain
-    generateRobotsTxt: true, // Generate robots.txt file
+    generateRobotsTxt: true,
+    exclude: ['/icon.png'], 
+    robotsTxtOptions: {
+        policies: [
+        { userAgent: '*', allow: '/' },
+        ],
+    }, // Generate robots.txt file
     changefreq: 'daily', // Optional: Frequency of content updates
     priority: 0.7, // Optional: Default priority
     sitemapSize: 5000, // Optional: Max entries per sitemap file
