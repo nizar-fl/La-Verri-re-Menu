971296db688799a38103dbd6faac79b733ef0c68
unknown <flahinounou@gmail.com>
2025-02-06 21:12:13 +0100

    changed the location

--- sections/footer/Footer.jsx
+++ sections/footer/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
                 <div className="location">
                 
                     <svg className="contactandlogoicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-                    <a href="https://maps.app.goo.gl/vM9pZtvziufvrFvA8" target="_blank" rel="noreferrer">El Menzah 9 , Ariana </a> 
+                    <a href="https://maps.app.goo.gl/vM9pZtvziufvrFvA8" target="_blank" rel="noreferrer">El Menzah 7 , Ariana </a> 
                 </div>
             </div>
             
