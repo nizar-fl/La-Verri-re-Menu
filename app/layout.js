ee5aa7d5b641be9dae150ab83424218e8efdc86d
unknown <flahinounou@gmail.com>
2025-02-17 20:25:14 +0100

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${cormorant.variable} antialiased`}
       >
-        {children}
+        {children}<Analytics />
       </body>
     </html>
   );
